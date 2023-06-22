59d33d2271e4be4d12ad45331b76a64155f70086
alwaysadevzero <119855949+alwaysadevzero@users.noreply.github.com>
2023-06-22 23:13:40 +0400

    refactor: disable sass in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -42,25 +42,25 @@ module.exports = {
                 test: /\.(woff(2)?|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                    {
-                        loader: "sass-resources-loader",
-                        options: {
-                            resources: [
-                                './src/styles/style.scss',
-                            ]
-                        }
-                    }
-                ],
-            },
+            // {
+            //     test: /\.s[ac]ss$/i,
+            //     use: [
+            //         // Creates `style` nodes from JS strings
+            //         "style-loader",
+            //         // Translates CSS into CommonJS
+            //         "css-loader",
+            //         // Compiles Sass to CSS
+            //         "sass-loader",
+            //         {
+            //             loader: "sass-resources-loader",
+            //             options: {
+            //                 resources: [
+            //                     './src/styles/style.scss',
+            //                 ]
+            //             }
+            //         }
+            //     ],
+            // },
         ],
     },
     plugins: [
