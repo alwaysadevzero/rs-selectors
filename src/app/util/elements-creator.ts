f1289e197a31a9e610504903aa4c0763ecf74365
Anthony <alwaysadevzero@gmail.com>
2023-06-24 09:21:40 +0400

    feat: add element-creator

---
+++ src/app/util/elements-creator.ts
@@ -0,0 +1,64 @@
+export {ComponentProps} from './types'
+
+class BaseComponent <T extends keyof HTMLElementTagNameMap = 'div'>{
+    public node: HTMLElement
+    constructor({
+      parent = null,
+      tag = 'div' as T,
+      className = '',
+      content = ''
+      }: Partial<ComponentProps<T>>) {
+  
+      this.node = document.createElement(tag)
+      this.node.className = className
+      this.node.innerHTML = content
+      if (parent) {
+        parent.append(this.node)
+      }
+    }
+  
+    remove(): void {
+      this.node.remove()
+    }
+  
+    appendTo(parent: HTMLElement | BaseComponent): void {
+      parent.append(this.node)
+    }
+  
+    append(...components: (HTMLElement | BaseComponent)[]): void {
+      const nodes = components.map((component) => (component instanceof HTMLElement ? component : component.node))
+      this.node.append(...nodes)
+    }
+  
+    addListener(eventName: keyof GlobalEventHandlersEventMap, callback: () => void): void {
+      this.node.addEventListener(eventName, callback)
+    }
+  
+    setAttributes(attributes: Record<string, string>): void {
+      Object.entries(attributes).forEach(([prop, value]) => this.node.setAttribute(prop, value))
+    }
+  
+    removeAttributes(...attributes: string[]): void {
+      attributes.forEach((attribute) => this.node.removeAttribute(attribute))
+    }
+  
+    setContent(content: string): void {
+      this.node.textContent = content
+    }
+  
+    addClass(...classNames: string[]): void {
+      this.node.classList.add(...classNames)
+    }
+  
+    removeClass(...classNames: string[]): void {
+      this.node.classList.remove(...classNames)
+    }
+  
+    toggleClass(className: string, state: boolean): void {
+      this.node.classList.toggle(className, state)
+    }
+  
+    get style(): CSSStyleDeclaration {
+      return this.node.style
+    }
+  }
