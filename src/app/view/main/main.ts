fd6f8d3f7b80d3261d7fa93d05a2103962af543d
Anthony <alwaysadevzero@gmail.com>
2023-06-24 09:48:12 +0400

    feat: add main component

---
+++ src/app/view/main/main.ts
@@ -0,0 +1,10 @@
+import BaseComponent from '../view'
+
+export default class mainView extends BaseComponent {
+    constructor() {
+        const params = {
+            className: 'container',
+        }
+        super(params)
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 import './styles/reset.css';
 import './styles/styles.css';
+import mainView from './app/view/main/main'
+
+class app {
+    constructor(){
+        this.configureView()
+    }
+    configureView(){
+        const view = new mainView();
+        const main = document.querySelector('.main')
+        if (main) main.append(view.node)
+    }
+}
+
+new app()
