6b91facd9baa62b28fa77d53ed546dab6c0db09f
Anthony <alwaysadevzero@gmail.com>
2023-06-26 06:42:59 +0400

    feat: add modules logic for css

--- src/app/view/main/editor/editor.ts
+++ src/app/view/main/editor/editor.ts
@@ -1,4 +1,4 @@
-import './editor.css'
+import styles from './editor.module.css'
 import BaseComponent from '../../view'
 import PanelTemplate from '../shared/panel-template'
 
@@ -13,7 +13,7 @@ export default class EditorView extends BaseComponent {
 
     configureView() {
 
-        const helpButton = new BaseComponent<'button'>({content: 'Help', tag: 'button', className: 'panel__button'})
+        const helpButton = new BaseComponent<'button'>({content: 'Help', tag: 'button', className: styles.panelButton})
         helpButton.setAttributes({'type': 'submit'})
 
         const editorHeader = new PanelTemplate('CSS Editor', '')
@@ -21,7 +21,7 @@ export default class EditorView extends BaseComponent {
         editorHeader.append(helpButton)
         // editorHeader.node.insertBefore(helpButton.node, editorHeader.node.querySelector('.panel__filename'));
 
-        const editorInput = new BaseComponent<'input'>({tag: 'input', className: 'editor__input'})
+        const editorInput = new BaseComponent<'input'>({tag: 'input', className: styles.panel__button})
         editorInput.setAttributes({placeholder: 'Enter css selector'})
         this.append(editorHeader, editorInput)
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,70 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-
 
 module.exports = {
-    //ничает сборку отсюда
-    entry: './src/index.ts', 
-    output: { //собранные файлы ложит сюда
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'index.js', 
-      assetModuleFilename: 'assets/[hash][ext]',
-    },
-
-    mode: 'development',
-    devServer: { //также можно указать dev server из модуля webpack-dev-server
-      compress: true,
-      open: true,
-      hot: true,
-      port: 3000,
-    },
-
-    module: { // 
-        rules: [
-            {
-                test: /\.ts$/, use: 'ts-loader',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  devtool: "inline-source-map",
+  mode: 'development',
+  devServer: {
+    compress: true,
+    open: true,
+    hot: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]_[hash:base64:5]', // Customize the generated class names
+              },
+              importLoaders: 2,
             },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            // {
-            //     test: /\.s[ac]ss$/i,
-            //     use: [
-            //         // Creates `style` nodes from JS strings
-            //         "style-loader",
-            //         // Translates CSS into CommonJS
-            //         "css-loader",
-            //         // Compiles Sass to CSS
-            //         "sass-loader",
-            //         {
-            //             loader: "sass-resources-loader",
-            //             options: {
-            //                 resources: [
-            //                     './src/styles/style.scss',
-            //                 ]
-            //             }
-            //         }
-            //     ],
-            // },
+          },
+          'sass-loader',
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        }),
-        new ESLintPlugin(),
-        // new CopyPlugin({
-        //     patterns: [
-        //       { from: "source", to: "dest" },
-        //       { from: "other", to: "public" },
-        //     ],
-        //   }),
+      },
     ],
-    resolve: {
-            extensions: ['.ts', '.js', '.json'],
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new ESLintPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
 };
