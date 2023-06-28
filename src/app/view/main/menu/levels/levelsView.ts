63d3971127b6abbc3dd23b0930e2b09a858426ae
alwaysadevzero <alwaysadevzero@gmail.com>
2023-06-29 03:59:09 +0400

    feat: add listener logic levelView

--- src/app/view/main/menu/levels/levelsView.ts
+++ src/app/view/main/menu/levels/levelsView.ts
@@ -2,44 +2,59 @@ import styles from "./levels.module.css";
 import View from "../../../view";
 import eventEmmiter from "../../../../util/eventEmmiter";
 import LevelView from "./level/level";
-import { levelParams } from "./level/typesView";
+import { levelParams } from "./types";
 import { LevelStatus } from "../../../../model/enums";
 
 export default class LevelsView extends View<"article"> {
+  private levels!: View;
+
   constructor() {
     super({
       className: styles.levels,
     });
     this.configureView();
+    this.addEventListeners();
+  }
+  addEventListeners(): void {
+    eventEmmiter.on(
+      eventEmmiter.events.DRAW_LEVELS,
+      this.drawLevels.bind(this)
+    );
   }
 
-  configureView() {
+  private configureView() {
     const heading = new View({ content: "Choose a level" });
 
-    const levels = this.configureLevels([
-      { status: LevelStatus.SKIPPED, levelName: "boban" },
-    ]);
+    this.levels = new View({ className: styles.levels });
 
     const resetButton = new View({
       className: styles.resetButton,
       tag: "button",
       content: "Reset Progress",
     });
-    this.append(heading, levels, resetButton);
+    this.append(heading, this.levels, resetButton);
+  }
+  
+  private isLevelParams(obj: unknown): obj is levelParams {
+    return (
+      typeof obj === "object" &&
+      obj !== null &&
+      "status" in obj &&
+      "levelName" in obj
+    );
   }
 
-  private configureLevels(levels: levelParams[]): View {
-    const levelsWrapper = new View({ className: styles.levels });
-
-    for (let i = 0; i < levels.length; i++) {
-      levels[i].position = i;
-
-      console.log(levels[i]);
-
-      const level = new LevelView(levels[i]);
-
-      levelsWrapper.append(level);
+  private drawLevels(levels: levelParams[]): void {
+    if (
+      !Array.isArray(levels) ||
+      !levels.every((level) => this.isLevelParams(level))
+    ) {
+      throw new Error("invalid passed parameters to drawLevels");
     }
-    return levelsWrapper;
+    while (this.levels.node.firstChild) {
+      this.levels.node.removeChild(this.levels.node.firstChild);
+    }
+
+    levels.forEach((level) => this.levels.append(new LevelView(level)))
   }
 }
