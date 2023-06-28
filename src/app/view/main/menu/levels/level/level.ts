57730e2b951c2d12929c1dd854538f55f4a8d1d7
Anthony <alwaysadevzero@gmail.com>
2023-06-28 20:30:52 +0400

    feat: add level component

---
+++ src/app/view/main/menu/levels/level/level.ts
@@ -0,0 +1,21 @@
+import styles from './level.module.css'
+import View from '../../../../view'
+import { levelParams } from './typesView'
+
+export default class LevelView extends View<'article'> {
+    constructor(params: levelParams) {
+        super({
+            className: styles.level
+        })
+        this.configureView(params)
+    }
+
+    configureView(params: levelParams) {
+        this.addClass(params.status)
+
+        const position = new View({content: params.position?.toString(), tag: 'span'})
+        const levelName = new View({tag: 'span',content: params.levelName})
+
+        this.append(position,levelName)
+    }
+}
