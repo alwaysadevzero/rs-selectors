bea59887fa2cc7751946998cff1d083e45061df2
Anthony <alwaysadevzero@gmail.com>
2023-06-28 06:18:28 +0400

    feat: move footer to menu component

--- src/app/view/main/main.ts
+++ src/app/view/main/main.ts
@@ -1,20 +1,56 @@
+import styles from './main.module.css'
 import View from '../view'
 import EditorView from './editor/editorView'
 import menuView from './menu/menuView'
+import parserHtml from '../../util/parserHtml'
+
+
+const footerRawHTML = `
+    <footer class="footer">
+        <div class="container">
+            <ul class="footer__links">
+                <li>
+                    <a href="https://rs.school/" class="contrast">© 2022 The Rolling Scopes</a>
+                </li>
+                <li>
+                    <a href="https://github.com/alwaysadevzero" class="contrast secondary">Author</a>
+                </li>
+                <li>
+                    <a href="" class="contrast">RS selectors 2023</a>
+                </li>
+            </ul>
+        </div>
+    </footer>`;
 
 export default class MainView extends View {
     constructor() {
         const params = {
-            className: 'container',
+            className: styles.app
         }
         super(params)
 
     
         this.configureView()
     }
-    configureView() {
+    private configureView(): void {
+
+        const leftSide = new View({className: styles.leftSide})
+
+        const title = new View({tag: 'h1', className: styles.title, content: "RS Selectors"})
         const editor = new EditorView()
+        const footer = parserHtml.parse(footerRawHTML)
+
+
+
+        leftSide.append(title, editor, footer)
+
+        
+        const rightSide = new View({className: styles.rightSide})
+
         const menu = new menuView()
-        this.append(editor, menu)
+
+        rightSide.append(menu)
+
+        this.append(leftSide,rightSide)
     }
 }
