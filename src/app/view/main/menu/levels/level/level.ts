6c8240800e7e4adc590b75e306ac12e17b14f963
alwaysadevzero <alwaysadevzero@gmail.com>
2023-06-29 03:58:24 +0400

    refactor: change className for level component

--- src/app/view/main/menu/levels/level/level.ts
+++ src/app/view/main/menu/levels/level/level.ts
@@ -1,6 +1,6 @@
 import styles from "./level.module.css";
 import View from "../../../../view";
-import { levelParams } from "./typesView";
+import { levelParams } from "../types";
 
 export default class LevelView extends View<"article"> {
   constructor(params: levelParams) {
@@ -12,12 +12,13 @@ export default class LevelView extends View<"article"> {
 
   configureView(params: levelParams) {
     this.addClass(params.status);
+    console.log(params);
 
     const position = new View({
       content: params.position?.toString(),
       tag: "span",
     });
-    const levelName = new View({ tag: "span", content: params.levelName });
+    const levelName = new View({ tag: "span", content: params.name });
 
     this.append(position, levelName);
   }
