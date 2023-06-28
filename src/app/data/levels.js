0e126b439d36e9b4d807679bb50b330d9cae0e65
Anthony <lokorti750@gmail.com>
2023-06-28 05:07:55 +0400

    feat: create stub level

---
+++ src/app/data/levels.js
@@ -0,0 +1,9 @@
+const levels = [
+    {
+        'id': '1',
+        'description': 'lalalala',
+        'name': 'ddadada',
+        'solution': '.da',
+        'html': '<plate></plate><plate><apple></apple></plate>'
+    }
+]
