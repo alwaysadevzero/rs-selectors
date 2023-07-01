f1191da1b1180565e932167db4262cd1ad7a535e
Anthony <alwaysadevzero@gmail.com>
2023-07-01 11:09:23 +0400

    feat: add  getWrap and const in parserHTML module

--- src/app/util/parserHtml.ts
+++ src/app/util/parserHtml.ts
@@ -1,13 +1,30 @@
-import sanitizeHtml from "sanitize-html";
+import sanitizeHtml, { IOptions } from "sanitize-html";
+
+const ALLOWED_TAGS = ["plate", "bento", "apple", "orange", "pickle"];
 class ParserHtml {
-  parse(htmlString: string): string {
-    const markup = sanitizeHtml(htmlString, {
-      allowedClasses: {
-        "*": ["*"],
-      },
-    });
-    console.log(markup);
+  public parse(htmlString: string, tags?: boolean): string {
+    let settings: Partial<IOptions> = {
+      allowedClasses: { "*": ["*"] },
+    };
+
+    if (tags) {
+      settings = { ...settings, allowedTags: ALLOWED_TAGS };
+    }
+
+    const markup = sanitizeHtml(htmlString, settings);
     return markup;
+  }
+
+  public getWrap(htmlString: string, tags?: boolean): HTMLElement {
+    const elem = document.createElement("div");
+
+    let wrap;
+    if (tags) wrap = this.parse(htmlString, tags);
+    else wrap = this.parse(htmlString);
+
+    elem.innerHTML = wrap;
+
+    return elem;
   }
 }
 
