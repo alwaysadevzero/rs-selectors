108dd5f8c428850f59e50a5f8df7d32861fe917d
alwaysadevzero <alwaysadevzero@gmail.com>
2023-07-02 19:51:45 +0400

    feat: bind progress logic with progress component

--- src/app/view/main/menu/menuView.ts
+++ src/app/view/main/menu/menuView.ts
@@ -1,11 +1,12 @@
 import styles from "./menu.module.css";
 import View from "../../view";
 import LevelsView from "./levels/levelsView";
-import DescriptionView from "./description/description";
 import eventEmmiter from "../../../util/eventEmmiter";
 
 export default class MenuView extends View<"article"> {
   private resetButton!: View;
+
+  private progress!: View;
 
   constructor() {
     super({
@@ -20,11 +21,28 @@ export default class MenuView extends View<"article"> {
     this.resetButton.addListener("click", () => {
       eventEmmiter.emit(eventEmmiter.events.RESET_LEVELS);
     });
+    eventEmmiter.on(eventEmmiter.events.DRAW_PROGRESS, this.updateProgress);
   }
 
+  private updateProgress = (params: {
+    index: number;
+    length: number;
+  }): void => {
+    const { index, length } = params;
+    this.progress.setAttributes({
+      value: `${index + 1}`,
+      max: `${length}`,
+    });
+  };
+
   private configureView(): void {
+    const header = new View({ className: styles.header });
     const levels = new LevelsView();
-    const description = new DescriptionView();
+    const progress = new View({ tag: "progress", className: styles.progress });
+    progress.setAttributes({ value: "0", max: "100" });
+
+    header.append(progress);
+    this.progress = progress;
 
     this.resetButton = new View({
       className: styles.resetButton,
@@ -32,6 +50,6 @@ export default class MenuView extends View<"article"> {
       content: "Reset Progress",
     });
 
-    this.append(description, levels, this.resetButton);
+    this.append(header, levels, this.resetButton);
   }
 }
